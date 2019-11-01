d82ee09c15c409368dafebcda9eddb01c462936a
Emmanuel Droz <emmanuel.droz@epfl.ch>
2019-11-01 14:34:46 +0100

    First version (still not an arduino library)

---
+++ base_rlduino/base_rlduino.ino
@@ -0,0 +1,56 @@
+// Arduino adapation of  Samoylov Eugene aka Helius microrl lib (ghelius@gmail.com)
+// GNU licence
+// E.Droz 1.11.2019
+
+#include "microrl.h"
+
+// create microrl object and pointer on it
+microrl_t rl;
+
+
+void setup()
+{
+   Serial.begin(115200);
+
+   Serial.print ("\n________________ microrl for Arduino _______________\n");
+   
+   
+   microrl_init (&rl);
+   microrl_set_execute_callback (&rl, command);
+}
+
+void loop()
+{
+
+   microrl_run(&rl);
+
+  // delay(10); 
+}
+
+
+#define HELP() if (strcmp(argv[0], "help")==0)
+
+
+int command (int argc, const char * const * argv)
+{
+      HELP(){//                 : 
+         Serial.print("hello          : Return world !\n");
+      }; 
+      if  (strcmp (argv[0], "hello") == 0) 
+      {
+            Serial.print("World ! \n"); 
+         return 0;
+      }
+     
+     
+            
+      HELP(){ return 0;}
+      
+      //error message if command was not used
+      //for (int i=0; i<argc;i++)printf("%s ",argv[i]);
+      Serial.print ("command not found or arguments error\n");
+}
+
+
+
+ 
